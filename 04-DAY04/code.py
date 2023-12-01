3bea3214fda58f304b07a3429993be1e64538cc9
Upendhar Nemmani <80301044+Upendhar10@users.noreply.github.com>
2023-12-01 14:31:06 +0530

    Create code.py

---
+++ 04-DAY04/code.py
@@ -0,0 +1,35 @@
+
+a = 1
+print(a)
+a1 = 2.0
+
+b = "Chintu"
+print(b)
+b1 = "Stylo"
+
+c = True
+print(c)
+
+d = None
+print(d)
+
+print(a+a1)     # Integer addition
+print(b+b1)     # String concatination
+
+print(type(a1))         # Number
+print(type(b1))         # str
+print(type(c))          # bool
+print(type(d))          # NoneType
+
+# List
+list1 = [8,12,True,"Chintu"]
+print(list1)
+
+# Tuple
+tuple1 = ("parrot","sparrow","lion")
+print(tuple1)
+
+# Dictionary
+
+dict1 = {"name": "Sakshi", "age":20, "canVote":True}
+print(dict1)
