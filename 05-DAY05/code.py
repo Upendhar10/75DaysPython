d629a71232b853f80be2733a47fc7f25972a952f
Upendhar Nemmani <80301044+Upendhar10@users.noreply.github.com>
2023-12-01 14:32:28 +0530

    Create code.py

---
+++ 05-DAY05/code.py
@@ -0,0 +1,19 @@
+
+# Taking Input in Python
+
+# a = input()
+# print(a)
+
+# b = input("Enter your name: ")
+# print(b)
+
+x = input("Enter first number: ")
+y = input("Enter second number: ")
+print(x + y)
+
+print(int(x) + int(y))
+
+print(int(x+y))
+
+
+    
