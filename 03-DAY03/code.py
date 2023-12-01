223be0f1db143c23f757195f7eba02cd117d54ba
Upendhar Nemmani <80301044+Upendhar10@users.noreply.github.com>
2023-12-01 14:29:32 +0530

    Create code.py

---
+++ 03-DAY03/code.py
@@ -0,0 +1,64 @@
+
+
+print("Hello World!")
+
+print(5)
+
+print("Bye")
+
+
+# Single-Line Comment
+
+# Example 1
+    
+print("This is a print statement.")
+
+# Output: This is a print statement.
+    
+# Example 2
+
+print("Hello World !!!")  #Printing Hello World
+
+# Output: Hello World !!!
+
+# Example 3:
+
+print("Python Program")
+#print("Python Program")
+
+# Output: Python Program
+
+'''
+This is a 
+Multi-line comment
+'''
+
+''' # Example 1 '''
+
+p = 7
+if (p > 5):
+    print("p is greater than 5.")
+else:
+    print("p is not greater than 5.")
+    
+# Output : p is greater than 5.
+
+#                   -------------------------------------- x ------------------------------------
+
+# Escape characters
+
+# print("This doesn't " execute")
+print("This will \" execute")
+
+print("Hello this how we insert \" in between \" ")
+
+
+print(5,6,7,sep="~")
+
+print(5,6,"hello",end=" 007")
+print("Mike")
+
+print(5,6,"hello",end=" 007\n")
+print("Mike")
+
+
